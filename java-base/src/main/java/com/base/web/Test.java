9a2ab682770c5866b6af1865204e0fa4d6b24a27
段志民 <duanzm@internal.ths.com.cn>
2021-09-12 09:03:21 +0800

    多余代码删除

---
+++ java-base/src/main/java/com/base/web/Test.java
@@ -0,0 +1,9 @@
+package com.base.web;
+
+public class Test {
+    public static void main(String[] args) {
+        String s = "a";
+        String s1 = "a";
+        System.out.println(s == s1);
+    }
+}
