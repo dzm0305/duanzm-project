e20a2d24bc5e72ba3ae517b5083318e240f2f782
duanzm <dzm0305@163.com>
2022-01-02 16:14:22 +0800

    登陆修改

---
+++ java-base/src/main/java/com/base/web/Test.java
@@ -0,0 +1,13 @@
+package com.base.web;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        for(int i = 0; i < nums.length; ++i){
+            System.out.println(i);
+            System.out.println("---------");
+            //System.out.println(nums[i]);
+        }
+    }
+
+}
