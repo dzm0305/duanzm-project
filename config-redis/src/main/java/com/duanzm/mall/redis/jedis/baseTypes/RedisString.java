c7348b3914fcf485a22b6580c05946d89bfe90a5
duanzm <dzm0305@163.com>
2022-10-08 23:39:38 +0800

    修改

---
+++ config-redis/src/main/java/com/duanzm/mall/redis/jedis/baseTypes/RedisString.java
@@ -0,0 +1,51 @@
+/*
+package com.duanzm.mall.redis.jedis.baseTypes;
+
+import com.duanzm.mall.redis.jedis.config.JedisConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+*/
+/**
+ * String类型 存取
+ *//*
+
+@Component
+public class RedisString {
+
+    @Autowired
+    private JedisPool jedisPool;
+
+    public void set(String key, String value){
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            jedis.set(key, value);
+            System.out.println("Redis 添加 " + key + " 成功");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+
+    public void get(String key){
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            String value = jedis.get(key);
+            System.out.println("Redis中 " + key + " 值为：" + value);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+}
+*/
